0945057f21ebcc5714fe951f0b0e2df299bff03f
Bagrielzin <gabriel.kratos2002@gmail.com>
2025-09-18 16:17:55 -0300

    feat: adiciona usuario

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+# app/models/user.py
+from sqlalchemy import Column, Integer, String
+from app.config import Base
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
